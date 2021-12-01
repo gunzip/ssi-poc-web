e37567e40742f8950ce99c63887cef27218af892
gunzip <gunzip@users.noreply.github.com>
2021-12-01 21:55:26 +0000

    deploy: e739e6f89dd756a9c60017f8e2c7d120601978e5

--- _next/static/Y9koAXtb1QfUbsRth9Jnv/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-16b595f1e8fcbab9.js"],"/_error":["static/chunks/pages/_error-d828d26f1f5d2954.js"],"/about":["static/chunks/pages/about-fc6a9c9cee0d5384.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
