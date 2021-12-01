c569ecae3c5f96790dd116aefff8ea26483478fd
danilo spinelli <danilo.spinelli@pagopa.itt>
2021-12-01 22:51:43 +0100

    chore: test deployment

--- pages/about.js
+++ pages/about.js
@@ -1,14 +1,14 @@
-import Link from 'next/link'
+import Link from "next/link";
 export default function About() {
   return (
     <div>
       <div>About us</div>
       <div>
-        Back to{' '}
-        <Link href="/" as={process.env.BACKEND_URL + '/'}>
-          <a>Home</a>
+        Back to{" "}
+        <Link href="/" as={process.env.BACKEND_URL + "/"}>
+          <a>About a boy</a>
         </Link>
       </div>
     </div>
-  )
+  );
 }
