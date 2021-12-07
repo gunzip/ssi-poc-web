00f741cad437ad4486434b48d6a952ce55b02490
danilo spinelli <danilo.spinelli@pagopa.itt>
2021-12-07 19:04:58 +0100

    fix: use image

--- pages/success.tsx
+++ pages/success.tsx
@@ -1,17 +1,16 @@
 import { NextPage } from "next";
 import Link from "next/link";
+import Image from "next/image";
+
 import { VStack, HStack } from "@chakra-ui/layout";
 
 const Success: NextPage = () => {
   return (
     <HStack spacing={10}>
       <VStack pt={20} spacing={10} width={["100%", "100%", "100%", "30%"]}>
-        <img
-          src="https://www.missioneparadiso.it/images/ndf/big/72.jpg"
-          width="400"
-        />
+        <Image src="/vampire.jpeg" width="400" height="350" />
         <Link href="/" as="/">
-          <a href="/">BACK</a>
+          <a>BACK</a>
         </Link>
       </VStack>
     </HStack>
