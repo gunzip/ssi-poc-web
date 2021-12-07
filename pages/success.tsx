225400be004c7047374b0a48d74a1f5bd80e688b
danilo spinelli <danilo.spinelli@pagopa.itt>
2021-12-07 19:10:46 +0100

    fix: next images

--- pages/success.tsx
+++ pages/success.tsx
@@ -1,6 +1,5 @@
 import { NextPage } from "next";
 import Link from "next/link";
-import Image from "next/image";
 
 import { VStack, HStack } from "@chakra-ui/layout";
 
@@ -8,7 +7,7 @@ const Success: NextPage = () => {
   return (
     <HStack spacing={10}>
       <VStack pt={20} spacing={10} width={["100%", "100%", "100%", "30%"]}>
-        <Image src="/vampire.jpeg" width="400" height="350" />
+        <img src="/vampire.jpeg" width="400" height="350" />
         <Link href="/" as="/">
           <a>BACK</a>
         </Link>
