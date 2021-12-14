7276e7741ca2b8333f1a8bca48a83090f142e68d
danilo spinelli <danilo.spinelli@pagopa.itt>
2021-12-14 11:16:24 +0100

    chore: add calendat

--- pages/success.tsx
+++ pages/success.tsx
@@ -1,16 +1,21 @@
 import { NextPage } from "next";
-
-import { VStack, HStack, Text } from "@chakra-ui/layout";
+import Calendar from "react-calendar";
+import { VStack, HStack, Text, Heading, Box } from "@chakra-ui/layout";
+import "react-calendar/dist/Calendar.css";
 
 const Success: NextPage = () => {
   return (
     <HStack spacing={10}>
       <VStack pt={10} spacing={10} width={["100%", "100%", "100%", "30%"]}>
+        <Heading>Prenota la tua donazione!</Heading>
+        <Box boxShadow="xl" rounded="md">
+          <Calendar />
+        </Box>
+        {/* <img src={`${assetPrefix}/vampire.jpeg`} width="400" height="350" /> */}
         <Text as="p" fontSize="xl" fontWeight="bold">
           Controlla il tuo Wallet per visualizzare le tue nuove credenziali
           AVIS!
         </Text>
-        {/* <img src={`${assetPrefix}/vampire.jpeg`} width="400" height="350" /> */}
       </VStack>
     </HStack>
   );
